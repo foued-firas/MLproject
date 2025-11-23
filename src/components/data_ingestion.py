fc01777ec044170f610b861ef6e16ee6e791ca40
Foued Firas <firas.fouad@isimg.tn>
2025-11-24 00:43:54 +0100

    Data ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -26,7 +26,7 @@ class DataIngestion:
             logging.info('Read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
-            df.to_csv(self.ingestion_config.train_data_path, index=False,header=True)
+            df.to_csv(self.ingestion_config.raw_data_path, index=False,header=True)
 
             logging.info("Train test split initiated")
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
@@ -40,3 +40,7 @@ class DataIngestion:
 
         except Exception as e:
             raise CustomException(e, sys)
+        
+if __name__=="__main__":
+    obj=DataIngestion()
+    obj.initiate_data_ingestion()
